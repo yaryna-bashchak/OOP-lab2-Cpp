a06df04bd60cbccf75de04394f326bfd0dc71071
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-10-12 22:28:22 +0300

    refactor class ShapeObjectsEditor

--- shape_objects_editor.cpp
+++ shape_objects_editor.cpp
@@ -3,4 +3,64 @@
 #include "editor.h"
 #include "shape.h"
 
-//and next methods
+ShapeObjectsEditor::ShapeObjectsEditor(int size, int* p) {
+	ARRAY_SIZE = size;
+	pCOUNT_OF_OBJECTS = p;
+};
+
+void ShapeObjectsEditor::StartPointEditor(HWND hWnd) {
+	SetWindowText(hWnd, L"Режим вводу крапок");
+	type = "point";
+};
+void ShapeObjectsEditor::StartLineEditor(HWND hWnd) {
+	SetWindowText(hWnd, L"Режим вводу ліній");
+	type = "line";
+};
+void ShapeObjectsEditor::StartRectEditor(HWND hWnd) {
+	SetWindowText(hWnd, L"Режим вводу прямокутників");
+	type = "rect";
+};
+void ShapeObjectsEditor::StartEllipseEditor(HWND hWnd) {
+	SetWindowText(hWnd, L"Режим вводу еліпсів");
+	type = "ellipse";
+};
+void ShapeObjectsEditor::OnLBdown(HWND hWnd, ShapeEditor* pse[]) {
+	if (type != "") {
+		if (type == "point") {
+			pse[0] = new PointEditor(hWnd);
+		}
+		else if (type == "line") {
+			pse[0] = new LineEditor(hWnd);
+		}
+		else if (type == "rect") {
+			pse[0] = new RectEditor(hWnd);
+		}
+		else if (type == "ellipse") {
+			pse[0] = new EllipseEditor(hWnd);
+		}
+		pse[0]->OnLBdown(hWnd);
+	}
+};
+void ShapeObjectsEditor::OnLBup(HWND hWnd, Shape* p[], ShapeEditor* pse[]) {
+	if (type != "") {
+		pse[0]->OnLBup(hWnd, p, *pCOUNT_OF_OBJECTS);
+		delete pse[0];
+		pse[0] = NULL;
+		InvalidateRect(hWnd, NULL, TRUE);
+		(*pCOUNT_OF_OBJECTS)++;
+	}
+};
+void ShapeObjectsEditor::OnMouseMove(HWND hWnd, ShapeEditor* pse[]) {
+	if (pse[0])
+		pse[0]->OnMouseMove(hWnd);
+};
+void ShapeObjectsEditor::OnPaint(HWND hWnd, Shape* p[]) {
+	//pse->OnPaint(hwnd, p);
+	PAINTSTRUCT ps;
+	HDC hdc;
+	hdc = BeginPaint(hWnd, &ps);
+	for (int i = 0; i < *pCOUNT_OF_OBJECTS; i++)
+		p[i]->Show(hdc);
+	EndPaint(hWnd, &ps);
+
+};
--- shape_objects_editor.h
+++ shape_objects_editor.h
@@ -11,67 +11,15 @@ class ShapeObjectsEditor
 private:
 	int* pCOUNT_OF_OBJECTS;
 	int ARRAY_SIZE;
-	//long xstart, ystart, xend, yend;
 	string type;
 public:
-	ShapeObjectsEditor(int size, int* p) {
-		ARRAY_SIZE = size;
-		pCOUNT_OF_OBJECTS = p;
-	};
-	void StartPointEditor(HWND hWnd) {
-		SetWindowText(hWnd, L"Режим вводу крапок");
-		type = "point";
-	};
-	void StartLineEditor(HWND hWnd) {
-		SetWindowText(hWnd, L"Режим вводу ліній");
-		type = "line";
-	};
-	void StartRectEditor(HWND hWnd) {
-		SetWindowText(hWnd, L"Режим вводу прямокутників");
-		type = "rect";
-	};
-	void StartEllipseEditor(HWND hWnd) {
-		SetWindowText(hWnd, L"Режим вводу еліпсів");
-		type = "ellipse";
-	};
-	void OnLBdown(HWND hWnd, ShapeEditor* pse[]) {
-		if (type != "") {
-			if (type == "point") {
-				pse[0] = new PointEditor(hWnd);
-			}
-			else if (type == "line") {
-				pse[0] = new LineEditor(hWnd);
-			}
-			else if (type == "rect") {
-				//p[COUNT_OF_OBJECTS] = new RectShape;
-			}
-			else if (type == "ellipse") {
-				//p[COUNT_OF_OBJECTS] = new EllipseShape;
-			}
-			pse[0]->OnLBdown(hWnd);
-		}
-	};
-	void OnLBup(HWND hWnd, Shape* p[], ShapeEditor* pse[]) {
-		if (type != "") {
-			pse[0]->OnLBup(hWnd, p, *pCOUNT_OF_OBJECTS);
-			delete pse[0];
-			pse[0] = NULL;
-			InvalidateRect(hWnd, NULL, TRUE);
-			(*pCOUNT_OF_OBJECTS)++;
-		}
-	};
-	void OnMouseMove(HWND hWnd, ShapeEditor* pse[]) {
-		if (pse[0])
-			pse[0]->OnMouseMove(hWnd);
-	};
-	void OnPaint(HWND hWnd, Shape* p[]) {
-		//pse->OnPaint(hwnd, p);
-		PAINTSTRUCT ps;
-		HDC hdc;
-		hdc = BeginPaint(hWnd, &ps);
-		for (int i = 0; i < *pCOUNT_OF_OBJECTS; i++)
-			p[i]->Show(hdc);
-		EndPaint(hWnd, &ps);
-		
-	};
+	ShapeObjectsEditor(int size, int* p);
+	void StartPointEditor(HWND hWnd);
+	void StartLineEditor(HWND hWnd);
+	void StartRectEditor(HWND hWnd);
+	void StartEllipseEditor(HWND hWnd);
+	void OnLBdown(HWND hWnd, ShapeEditor* pse[]);
+	void OnLBup(HWND hWnd, Shape* p[], ShapeEditor* pse[]);
+	void OnMouseMove(HWND hWnd, ShapeEditor* pse[]);
+	void OnPaint(HWND hWnd, Shape* p[]);
 };
